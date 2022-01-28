cc2a107f29dc0527b6e4a3f35b050de10bd2d1c0
paullaster <paullasterokoth98@gmail.com>
2022-01-28 23:03:02 +0300

    vonage_API

--- config/prod_config.js
+++ config/prod_config.js
@@ -2,4 +2,6 @@ module.exports = {
     PORT : process.env.PORT,
     DB_URI: process.env.DB_URI,
     SESSION_SECRET: process.env.SESSION_SECRET,
+    API_KEY: process.env.API_KEY,
+    API_SECRET: process.env.API_SECRET,
 }
