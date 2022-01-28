6b967129fd1f5309d7d6f54f8e8a6eaeb14a6cb4
timothyAgevi <timothyagevi@gmail.com>
2022-01-29 00:41:25 +0300

    update config

---
+++ config/dev_config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    PORT : 1234,
+    DB_URI:"mongodb+srv://alert:trela@emergencyalert.8f8bq.mongodb.net/emergencyAlert?retryWrites=true&w=majority",
+    SESSION_SECRET: 'adkskskdjdjf',
+    API_KEY: 'f379aa42',
+    API_SECRET: 'n0LKhsIrIufyfHQE',
+}
